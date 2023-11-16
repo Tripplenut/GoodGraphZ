c3bd0753cc6494f0b8787edc411bfc485f455083
Tripplenut <afabius05@gmail.com>
2023-11-15 23:12:19 -0500

    Refactoring Edges
    No this doesn't work yet

--- include/Graphs/Edges.hpp
+++ include/Graphs/Edges.hpp
@@ -1,7 +1,7 @@
 /**
  * @file Edges.hpp
  * @author Anthony Fabius
- * @brief This is where all things Edge related is stored
+ * @brief This is where all things Edge related are stored
  * @date 2023-11-11
  * 
  * @copyright Copyright (c) 2023
@@ -24,14 +24,15 @@ class Node;
 // GGZ includes
 #include "Nodes.hpp"
 
+/*****************************************************************************/
+
 /**
- * @brief The main Edge class
+ * @brief The parent Edge class
  * @tparam T The weight type
  */
-template <class T>
-class Edge{
+template<class T>
+class Edge {
 private:
-
   template <class N, class E>
   friend class Graph;
 
@@ -39,45 +40,121 @@ private:
   friend class Node;
 
   int id;
-  T data;
-  std::string sourceNodeId;
-  std::string targetNodeId;
-  bool directed; // If true edge is directed
-  bool weighted; // If true edge is weighted
 
   /**
-   * @brief Constructor for Unweighted Edges
-   * @param id_ Id of this edge
-   * @param source Source Node
-   * @param target Target Node
-   * @param isDirected Sets if edge is directed or not
-  */
-  Edge(int id_, std::string& source, std::string& target, bool isDirected){
-    // Make sure the edge isn't pointing to the same node
+   * @brief Constructs an unweighted Edge
+   * @param id_ Id of the Edge
+   */
+  Edge(int id_){
     id = id_;
-    sourceNodeId = source;
-    targetNodeId = target;
-    weighted = false;
-    directed = isDirected;
+    weight = NULL;
   }
 
   /**
-   * @brief Constructor for Weighted Edges
-   * @param id Id of this edge
-   * @param source Source Node
-   * @param target Target Node
-   * @param data_ Data
-   * @param isDirected Sets if edge is directed or not
-  */
-  Edge(int id_, std::string& source, std::string& target, T data_, bool isDirected){
-    // Make sure the edge isn't pointing to the same node
+   * @brief Constructs a weighted Edge
+   * @param id_ Id of the Edge
+   * @param weight_ The weight of the Edge
+   */
+  Edge(int id_, T weight_){
     id = id_;
-    sourceNodeId = source;
-    targetNodeId = target;
-    data = data_;
-    weighted = true;
-    directed = isDirected;
+    weight = weight_;
+  };
+
+public:
+
+  T weight;
+
+  /**
+   * @brief Sets the weight of the edge
+   * @param weight_ 
+   */
+  void setWeight(T weight_){
+    weight = weight_;
+  }
+
+  /**
+   * @brief Gets the id of the edge
+   * @return int 
+   */
+  int getId() const {
+    return id;
+  }
+
+  /**
+   * @brief Gets the weight of the edge
+   * @return The Weight
+   */
+  T& getWeight() const {
+    return weight;
   }
 
 };
+
+/*****************************************************************************/
+
+/**
+ * @brief The undirected edge class
+ * @tparam T The weight type
+ */
+template<class T>
+class UndirectedEdge : private Edge<T> {
+private:
+
+  using Edge<T>::Edge;
+
+  //! May need to include friends, idk yet
+
+  std::string node1Id;
+  std::string node2Id;
+
+  /**
+   * @brief Construct an Undirected Edge
+   * @param id_ Id of the Edge
+   * @param node1_ The id of Node1
+   * @param node2_ The id of Node2
+   */
+  UndirectedEdge(int id_, std::string node1_, std::string node2_) : Edge(id_) {
+    node1Id = node1Id_;
+    node2Id = node2Id_;
+  }
+
+  /**
+   * @brief Construct a weighted Undirected Edge
+   * @param id_ Id of the Edge
+   * @param weight_ The weight of the Edge
+   * @param node1_ The id of Node1
+   * @param node2_ The id of Node2
+   */
+  UndirectedEdge(int id_, T weight_, std::string node1_, std::string node2_){
+    id = id;_
+    weight = weight_;
+    node1Id = node1Id_;
+    node2Id = node2Id_;
+  }
+
+public:
+
+  /**
+   * @brief Retrieves id of node1
+   * @return std::string& 
+   */
+  std::string& getNode1() const {
+    return node1Id;
+  }
+
+  /**
+   * @brief Retrieves id of node2
+   * @return std::string& 
+   */
+  std::string& getNode2() const {
+    return node2Id;
+  }
+
+};
+
+/*****************************************************************************/
+
+// Todo Directed Edge Child Class goes here
+
+
 #endif
