6840849dea0af84198804435d861a5e0217fc401
Tripplenut <afabius05@gmail.com>
2023-11-18 13:20:01 -0500

    Some node and edge functions
    Doesn't really do anything yet. Still figuring out a bunch of stuff but
    this is working (so far)

--- include/Graphs/UndirectedGraph.hpp
+++ include/Graphs/UndirectedGraph.hpp
@@ -11,6 +11,8 @@
 #ifndef UNDIRECTEDGRAPHS_HPP
 #define UNDIRECTEDGRAPHS_HPP
 
+#include "stddef.h"
+
 // CPP includes
 #include <iostream>
 #include <string>
@@ -33,7 +35,7 @@ class UndirectedGraph {
 private:
 
   std::unordered_map<std::string, UndirectedNode<N>*> Nodes;
-  std::unordered_map<std::string, UndirectedEdge<E>*> Edges;
+  std::unordered_map<int, UndirectedEdge<E>*> Edges;
 
   bool weighted;
 
@@ -46,6 +48,59 @@ public:
   UndirectedGraph(bool weighted_ = false){
     weighted = weighted_;
   }
+
+  /**
+   * @brief Destroys an Undirected Graph
+   */
+  ~UndirectedGraph(){
+    for(auto iter = Nodes.begin(); iter != Nodes.end(); iter++){
+      delete iter->second;
+    }
+
+    for(auto iter = Edges.begin(); iter != Edges.end(); iter++){
+      delete iter->second;
+    }
+  }
+
+  /**
+   * @brief Adds a node to the graph
+   * @param nodeId The name of the node
+   * @param weight Optional weight for the node
+   * @return true If node was successfully added
+   * @return false If node could not be added
+   */
+  bool addNode(std::string nodeId){
+    //! Gonna need some error checking
+    if (Nodes.count(nodeId) != 0) // make sure nodes don't already exist
+      return false;
+    
+    Nodes[nodeId] = new UndirectedNode<N>(nodeId);
+    return true;
+  }
+
+  //TODO add weighted nodes
+
+  /**
+   * @brief Adds an edge to the graph
+   * @param node1Id The id of the first node
+   * @param node2Id The id of the second node
+   * @return true If edge was successfully added
+   * @return false If edge could not be added
+   */
+  bool AddEdge(std::string node1Id, std::string node2Id){
+    //! Gonna need a bunch of error checking (edge doesn't already exist)
+    if(node1Id == node2Id) // Make sure nodes don't point to themselves
+      return false;
+    
+    addNode(node1Id);
+    addNode(node2Id);
+
+    Edges[Edges.size()] = new UndirectedEdge<E>(Edges.size(), node1Id, node2Id);
+
+    return true;
+  }
+
+  //TODO add weighted edges
 
 };
 
--- include/Graphs/UndirectedNode.hpp
+++ include/Graphs/UndirectedNode.hpp
@@ -34,7 +34,13 @@ template <class T>
 class UndirectedNode{
 private:
 
-  //! Store Edges
+  template <class N, class E>
+  friend class UndirectedGraph;
+
+  template <class N>
+  friend class UndirectedEdge;
+
+  std::unordered_map<std::string, int> Edges;
   std::string id;
 
   /**
@@ -55,6 +61,17 @@ private:
     weight = weight_;
   }
 
+  /**
+   * @brief Adds Edge to node
+   * @param otherNodeId The id of the other node
+   * @param edgeId The id of the edge
+   * @return true If edge was successfully added
+   * @return false If edge could not be added
+   */
+  bool addEdge(std::string otherNodeId, int edgeId){
+    //! Implement
+  }
+
 public:
 
   T weight;
@@ -65,6 +82,41 @@ public:
    */
   T& getWeight() const {
     return weight;
+  }
+
+  /**
+   * @brief Gets the edge id from a node.
+   * Returns -1 if Edge does not exist.
+   * @param nodeId The NodeId 
+   * @return int 
+   */
+  int getEdgeId(std::string& nodeId){
+    return (Edges.count(nodeId) == 0) ? (-1) : Edges[nodeId];
+  }
+
+  /**
+   * @brief Gets all the ids of all the edges of a node.
+   * Puts them in a vector and returns it.
+   * @return std::vector<int> 
+   */
+  std::vector<int> getEdgesIds(){
+    std::vector<int> res;
+    for(auto iter = Edges.begin(); iter != Edges.end(); iter++){
+      res.push_back(iter->second);
+    }
+    return res;
+  }
+
+  /**
+   * @brief Gets all the nodeIds that this node shares an edge with.
+   * @return std::vector<std::string> 
+   */
+  std::vector<std::string> getEdgeNodes(){
+    std::vector<std::string> res;
+    for(auto iter = Edges.begin(); iter != Edges.end(); iter++){
+      res.push_back(iter->first);
+    }
+    return res;
   }
 
 };
